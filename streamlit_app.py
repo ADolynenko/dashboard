87a104966f1cc0d432fd405d18d9c0478c27484f
Antonina Dolynenko <sba24106@student.cct.ie>
2024-12-23 22:51:51 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -161,7 +161,7 @@ EDA['apparent_milk_yield'] = EDA['milk_yield'] * 1000
 EDA['milk_prod_mln'] = EDA['values_milk_on_farms'] / 1000
 dairy_cows_range = np.linspace(min(EDA['values_num_cows']), max(EDA['values_num_cows']), 100)
 milk_production_curve = (dairy_cows_range / 1000) * (EDA['apparent_milk_yield'].mean() / 1000)
-marker_size = (EDA['values'] / EDA['values'].max()) * 50
+marker_size = (EDA['values'] / EDA['values'].max()) * 30
 
 
 # Create Plotly figure
