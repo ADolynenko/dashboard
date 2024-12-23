d942d22a2816f63f3e9923326f6fd8568ede907c
Antonina Dolynenko <sba24106@student.cct.ie>
2024-12-23 23:19:39 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -183,6 +183,19 @@ fig.add_trace(go.Scatter(
     name='Countries'
 ))
 
+for index, row in EDA.iterrows():
+    if row['geo'] in ['IE', 'DK', 'NL']:
+        fig.add_annotation(
+            x=row['values_num_cows'],
+            y=row['milk_prod_mln'],
+            text=row['geo'],
+            showarrow=True,
+            arrowhead=2,
+            ax=row['values_num_cows'] * 0.05,  # Adjust arrow x-offset
+            ay=row['milk_prod_mln'] * 0.05,  # Adjust arrow y-offset
+            font=dict(size=10)
+        )
+
 # Add line for average milk yield
 fig.add_trace(go.Scatter(
     x=dairy_cows_range,
