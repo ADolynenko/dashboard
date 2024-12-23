c75648bae4ca37011c7a97a70518d7bdade8f4fb
Antonina Dolynenko <sba24106@student.cct.ie>
2024-12-23 22:40:12 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -160,7 +160,7 @@ EDA['apparent_milk_yield'] = EDA['milk_yield'] * 1000
 # Calculate additional variables
 EDA['milk_prod_mln'] = EDA['values_milk_on_farms'] / 1000
 dairy_cows_range = np.logspace(0.5, 3.65, 100)
-milk_production_curve = (dairy_cows_range / 1000) * (apparent_milk_yield.mean() / 1000)
+milk_production_curve = (dairy_cows_range / 1000) * (EDA['apparent_milk_yield'].mean() / 1000)
 marker_size = (EDA['values'] / EDA['values'].max()) * 70
 
 
