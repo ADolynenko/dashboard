c42936a9417083516d37e3d568e2c996bd241a1c
Antonina Dolynenko <sba24106@student.cct.ie>
2024-12-24 12:42:46 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -100,7 +100,7 @@ fig2.update_layout(
 
 # Adding annotations for non-zero price changes
 for i, row in data_ie.iterrows():
-    if row['price_change'] != np.nan:        
+    if pd.notna(row['price_change']) and row['price_change'] != 0:        
         fig2.add_annotation(
                 x=row['time'],
                 y=row['price_change'],
